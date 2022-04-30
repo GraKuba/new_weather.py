a4547bfd1926d80a2a3ce72c2bb23c7b15bc0706
Jakub <grabarczyk.kuba@gmail.com>
2022-04-30 17:14:31 +0200

    Bugs and mistakes in classess fixed, all required functions added in weather.py

--- classes.py
+++ classes.py
@@ -1,10 +1,31 @@
 import requests
 import json
+from datetime import datetime
+
+# CLASSES AND FUNCTIONS
 
 
 class WeatherForecast:
 	def __init__(self, api_key):
 		self.api_key = api_key
+
+	def __getitem__(self, item):
+		return check_if_rain(item)
+
+	def __iter__(self):
+		print("All known days:")
+		known_days = []
+		imported_days = download_from_json()
+		for idx in imported_days:
+			for day in idx:
+				known_days.append(day)
+		return iter(known_days)
+
+	def items(self):
+		imported_days = download_from_json()
+		for idx in imported_days:
+			for day in idx:
+				yield day, idx[day]
 
 	def download_from_api(self):
 
@@ -34,3 +55,43 @@ class WeatherForecast:
 
 		with open('weather_history.json', 'w') as f:
 			json.dump(weather_history, f)
+
+
+def download_from_json():
+	with open('weather_history.json', 'r') as f:
+		data = json.load(f)
+
+	saved_days = []
+	rainy_days = []
+	saved_weather = []
+	for idx in data:
+		for date in idx:
+			day = datetime.fromtimestamp(int(date)).date()
+			if day not in saved_days:
+				saved_days.append(day)
+			if idx[date] == "Rain":
+				rainy_days.append(day)
+	for idx in saved_days:
+		for day in rainy_days:
+			if idx == day:
+				dct = {idx: "Rain"}
+				saved_weather.append(dct)
+			else:
+				dct = {idx: "No rain"}
+				saved_weather.append(dct)
+	return saved_weather
+
+
+def check_if_rain(requested_date):
+	saved_weather = download_from_json()
+	day_count = 0
+	counter = 0
+	for idx in saved_weather:
+		for day in idx:
+			day_count += 1
+			if requested_date == str(day):
+				return idx[day]
+			else:
+				counter += 1
+	if day_count == counter:
+		return "Don't know"
--- weather.py
+++ weather.py
@@ -7,3 +7,11 @@ if os.path.exists('weather_history.json') is False:
     open('weather_history.json', 'w')
     wf.download_from_api()
 
+
+print(wf['2022-04-29'])           # def __getitem__(self):
+
+for date in wf:                   # def __iter__(self):
+    print(date)
+
+for date, weather in wf.items():  # def items(self)
+    print(date, weather)
